037d2aafcee655ce94bea68ad5624a73658f03d1
Leckller <7leckler@gmail.com>
2023-07-28 19:44:16 -0300

    misturado

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ BuscaValorDeBolas.addEventListener('click', (e) => {
 })
 
 function muitaBola () {
-    for(let i = 0; i <= 2; i+= 1) {
+    for(let i = 0; i <= 50; i+= 1) {
         ctx.beginPath()
         ctx.fillStyle = `rgba(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, 0.9)`
         ctx.arc(Math.random()*canvas.width, Math.random()*canvas.height, Math.random()* 90 + 10, formulaRadGrau*0, formulaRadGrau*360, false)
@@ -33,7 +33,6 @@ function muitaBola () {
     ctx.fill()
     grauI += 1
     if(grauI === 360) ctx.clearRect(550, 100, 500, 500), grauI = 0
-    console.log(grauI)
     inicia = requestAnimationFrame(muitaBola)
 }
 
@@ -53,53 +52,54 @@ function muitaBola () {
 //         ctx.stroke()
 //     }
 // }
-// let direcao
-// let rX = 0
-// let rY = 0
-// let size = 50
-// let move = size
-// let orloff
-// const direcoes = ['direita', 'esquerda', 'cima', 'baixo']
+let direcao
+let rX = 0
+let rY = 0
+let size = 50
+let move = size
+let orloff
+const direcoes = ['direita', 'esquerda', 'cima', 'baixo']
 
-// function randomDirections () {
-//     return direcao = direcoes[Math.floor(Math.random() * 4)]
-// }
+function randomDirections () {
+    return direcao = direcoes[Math.floor(Math.random() * 4)]
+}
 
-// function player() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height)
-//     grid()
-//     ctx.fillRect(rX, rY, size, size)
+function player() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // grid()
+    ctx.fillStyle = 'white'
+    ctx.fillRect(rX, rY, size, size)
 
-//     ctx.lineWidth = 3
-//     ctx.strokeStyle = 'red'
-//     ctx.strokeRect(rX, rY, size, size)
+    ctx.lineWidth = 3
+    ctx.strokeStyle = 'red'
+    ctx.strokeRect(rX, rY, size, size)
 
-//     if (direcao === 'esquerda' && rX === 0) direcao = 'parado'
-//     if (direcao === 'direita' && rX === canvas.width - size) direcao = 'parado'
-//     if (direcao === 'cima' && rY === 0) direcao = 'parado'
-//     if (direcao === 'baixo' && rY === canvas.height - size) direcao = 'parado'
+    if (direcao === 'esquerda' && rX === 0) direcao = 'parado'
+    if (direcao === 'direita' && rX === canvas.width - size) direcao = 'parado'
+    if (direcao === 'cima' && rY === 0) direcao = 'parado'
+    if (direcao === 'baixo' && rY === canvas.height - size) direcao = 'parado'
 
-//     body.addEventListener('keyup', ({ key }) => {
-//         if (key === 'r') direcao = randomDirections()
-//         if (key === 'ArrowLeft') direcao = 'esquerda', move = 50;
-//         if (key === 'ArrowRight') direcao = 'direita', move = 50;
-//         if (key === 'ArrowUp') direcao = 'cima', move = 50; 
-//         if (key === 'ArrowDown') direcao = 'baixo', move = 50;
-//         if (key === ' ') direcao = 'parado'
-//       })
+    body.addEventListener('keyup', ({ key }) => {
+        if (key === 'r') direcao = randomDirections()
+        if (key === 'ArrowLeft') direcao = 'esquerda', move = 50;
+        if (key === 'ArrowRight') direcao = 'direita', move = 50;
+        if (key === 'ArrowUp') direcao = 'cima', move = 50; 
+        if (key === 'ArrowDown') direcao = 'baixo', move = 50;
+        if (key === ' ') direcao = 'parado'
+      })
 
-//     if (direcao === 'parado') rX += 0, rY += 0
-//     if (direcao === 'esquerda') rX += -move
-//     if (direcao === 'direita') rX += move
-//     if (direcao === 'cima') rY += -move
-//     if (direcao === 'baixo') rY += move
+    if (direcao === 'parado') rX += 0, rY += 0
+    if (direcao === 'esquerda') rX += -move
+    if (direcao === 'direita') rX += move
+    if (direcao === 'cima') rY += -move
+    if (direcao === 'baixo') rY += move
 
-//     orloff = setTimeout(() => {
-//         player()
-//     }, 50)
-// }
+    orloff = setTimeout(() => {
+        player()
+    }, 50)
+}
 
-// window.onload = () => {
-//     player()
-// }
+window.onload = () => {
+    player()
+}
 
